77b58b6243eb518842df28034f15fcfe9685c313
threedliams <threedliams@gmail.com>
2020-01-27 22:14:00 -0800

    add ability to check folders recursively, console colors

--- chode.js
+++ chode.js
@@ -1,16 +1,47 @@
 const fs = require('fs');
 
-const filename = process.argv[2];
+const path = process.argv[2];
 const encoding = process.argv[3] ? process.argv[3] : 'utf-8';
 
-let text = '';
-fs.readFile(filename, encoding, (e, rawData) => {
-    if (e) {
-        throw e;
+let problemFiles = [];
+
+let isValid = true;
+if (fs.lstatSync(path).isDirectory()) {
+    isValid = recurseFileTree(path)
+} else {
+    isValid = analyzeFile(path)
+}
+
+console.log();
+if (isValid) {
+    console.log('\x1b[32mOverall status: Valid!\x1b[0m');
+    process.exit(0);
+} else {
+    console.log('\x1b[31mOverall status: Rejected.\x1b[0m');
+    console.log('\x1b[31mProblem files:\x1b[0m');
+    problemFiles.forEach(problemFile => {
+        console.log('\x1b[31m' + problemFile + '\x1b[0m');
+    });
+    process.exit(1);
+}
+
+function recurseFileTree(path) {
+    let isValid = true;
+
+    if (fs.lstatSync(path).isDirectory()) {
+        fs.readdirSync(path).forEach(subPath => {
+            isValid = isValid & recurseFileTree(path + '/' + subPath);
+        });
+    } else {
+        isValid = analyzeFile(path);
     }
 
-    text = rawData.toString(encoding);
+    return isValid;
+}
 
+function analyzeFile(filename) {
+    const rawData = fs.readFileSync(filename, encoding);
+    const text = rawData.toString(encoding);
     const textLines = text.split("\n");
 
     let maxLineLength = 0;
@@ -20,12 +51,15 @@ fs.readFile(filename, encoding, (e, rawData) => {
         }
     });
 
+    console.log(filename + ":");
     console.log(textLines.length + " lines long.");
     console.log(maxLineLength + " columns wide.");
+
     if (maxLineLength > textLines.length) {
-        console.log('Chode.js approved!');
-    } else {
-        console.log('Rejected - too long.');
-        process.exit(1);
+        console.log('\x1b[32mChode.js approved!\x1b[0m');
+        return true;
     }
-});
+    console.log('\x1b[31mRejected - too long.\x1b[0m');
+    problemFiles.push(filename);
+    return false;
+}
